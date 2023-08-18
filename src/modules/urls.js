88ef2b057900c9f3d19ca4047fc344352e9b5acb
YousofSayed <yousef.sayed1231@gmail.com>
2023-08-18 12:27:37 -0700

    upd7

--- src/modules/urls.js
+++ src/modules/urls.js
@@ -2,7 +2,7 @@ export default {
     surat:'../jsons-files/surat.json',
     ayahEndpoint:'https://api.alquran.cloud/v1/ayah',
     surahEndpoint:'https://api.alquran.cloud/v1/surah',
-    tafseerEndpoint:'https://localhost:7070/getTafseer',
+    tafseerEndpoint:'https://proxy-tafseer.cyclic.cloud/getTafseer',
     suraAudio:'https://cdn.islamic.network/quran/audio-surah',
     readersNames:'https://api.alquran.cloud/v1/edition/format/audio',
     // https://api.alquran.cloud/v1/ayah/262
